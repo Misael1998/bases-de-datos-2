7714ee0b435ede2936db52188f7c5c547c238350
Alexander Tejeda <AlexanderTejedaBarahona10@gmail.com>
2019-11-25 21:13:35 -0600

    MOdificacion de funcion, el problema es que quiero que en una sola linea envie la informacion del equipo del cual se envio el id y la otra informacion es sobre la informacion del eqequpo contra el que jugo

--- FuncionesTabla_1_3/FT_Ultimos10Partidos.sql
+++ FuncionesTabla_1_3/FT_Ultimos10Partidos.sql
@@ -20,53 +20,77 @@ RETURN
 	SELECT 0
 )
 
--- ------------------------------------------
--- DECLARACION DE VARIABLES USADAS DENTRO DE LA FUNCION
--- ------------------------------------------
-	DECLARE @nombreDeporte VARCHAR(50);
--- ------------------------------------------
--- CONSULTA PARA TRAER EL DEPORTE EN EL QUE ESTA ESTE EQUIPO
--- ------------------------------------------
-SELECT 
-	@nombreDeporte = depo.nombre
-	FROM [ScriptProyecto].[dbo].[Equipo] equi 
-	INNER JOIN [ScriptProyecto].[dbo].[Deporte] depo ON depo.idDeporte = equi.idDeporte
-	WHERE depo.idDeporte = 1
 
-	IF(UPPER(@nombreDeporte) = 'BALONCESTO' or UPPER(@nombreDeporte) = 'BASKETBALL')
-		BEGIN
-						
-		END
+-- ----------------------------------
+-- DECLARACION DE VARIABLES
+-- ----------------------------------
+
+DECLARE @idPartido INT; 
+
+DECLARE idUltimosPartidos SCROLL CURSOR
+	FOR	SELECT 
+		TOP 10
+		part.idPartido
+			FROM [ScriptProyecto].[dbo].[Equipo] equi 
+			INNER JOIN [ScriptProyecto].[dbo].[EstadisticaBaloncesto] esba ON equi.idEquipo = esba.idEquipo
+			INNER JOIN [ScriptProyecto].[dbo].[Partido] part ON part.idEstadisticasBaloncesto = esba.idEstadisticasBaloncesto
+
+OPEN idUltimosPartidos
+
+FETCH FIRST FROM idUltimosPartidos INTO @idPartido 
+
+--WHILE(@@FETCH_STATUS = 0)
+	--BEGIN 
+	SELECT
+		SUM(parc.anotaciones) as anotacionesRiva,
+		equi.nombre 
+			FROM [ScriptProyecto].[dbo].[Equipo] equi
+			INNER JOIN [ScriptProyecto].[dbo].[EstadisticaBaloncesto] esba ON esba.idEquipo = equi.idEquipo
+			INNER JOIN [ScriptProyecto].[dbo].[Partido] part ON part.idEstadisticasBaloncesto = esba.idEstadisticasBaloncesto
+			INNER JOIN [ScriptProyecto].[dbo].[Parcial] parc ON parc.idEstadisticasBaloncesto = esba.idEstadisticasBaloncesto
+			WHERE equi.idEquipo = 1 and part.idPartido = 1
+				GROUP BY equi.nombre,
+	UNION 
+	SELECT 
+		equi1.nombre as EquipoRival,
+		SUM(parc.anotaciones) scoreRival
+			FROM [ScriptProyecto].[dbo].[Equipo] equi1
+			INNER JOIN [ScriptProyecto].[dbo].[EstadisticaBaloncesto] esba ON esba.idEquipo = equi1.idEquipo
+			INNER JOIN [ScriptProyecto].[dbo].[Parcial] parc ON parc.idEstadisticasBaloncesto = esba.idEstadisticasBaloncesto
+			INNER JOIN [ScriptProyecto].[dbo].[Partido] part ON part.idEstadisticasBaloncesto = esba.idEstadisticasBaloncesto
+			WHERE equi1.idEquipo <> 1 and part.idPartido = 1
+				GROUP BY  equi1.nombre
+	--END
+
+	DEALLOCATE idUltimosPartidos
 
 
 
-		esba 
 
 
 
-SELECT 
-	equ.nombre,
-	COUNT(parc.anotaciones) totalEquipo
-	FROM [ScriptProyecto].[dbo].[Partido] par
-	INNER JOIN [ScriptProyecto].[dbo].[EstadisticaBaloncesto] esba ON esba.idEstadisticasBaloncesto = par.idEstadisticasBaloncesto	
-	INNER JOIN [ScriptProyecto].[dbo].[Parcial] parc ON parc.idEstadisticasBaloncesto = esba.idEstadisticasBaloncesto
-	INNER JOIN [ScriptProyecto].[dbo].[Equipo] equ ON equ.idEquipo = esba.idEquipo
-	INNER JOIN 
-	(
-		SELECT 
-			par.idPartido,
-			equi.nombre as nombreRival,
-			COUNT(parc.anotaciones) as totalRival
-				FROM [ScriptProyecto].[dbo].[Partido] par
-				INNER JOIN [ScriptProyecto].[dbo].[EstadisticaBaloncesto] esba ON esba.idEstadisticasBaloncesto = par.idEstadisticasBaloncesto
-				INNER JOIN [ScriptProyecto].[dbo].[Equipo] equi ON equi.idEquipo = esba.idEquipo
-				INNER JOIN [ScriptProyecto].[dbo].[Parcial] parc ON par.idEstadisticasBaloncesto = esba.idEstadisticasBaloncesto
-						WHERE equi.idEquipo <> 1
-						GROUP BY equi.nombre,par.idPartido
-					 
-	) s1 ON s1.idPartido = par.idPartido
-		WHERE equ.idEquipo = 1 
-			GROUP BY equ.idEquipo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
